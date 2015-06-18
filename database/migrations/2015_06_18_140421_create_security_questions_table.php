21406ef4a9350955cc84dec7371da16bb341a1ae
jahidul <jsec516@gmail.com>
2015-06-18 20:08:26 +0600

    security question and user table updated

---
+++ database/migrations/2015_06_18_140421_create_security_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSecurityQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('security_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->integer('clinic_id')->unsigned();
+            $table->integer('clinic_user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('security_questions');
+    }
+}
