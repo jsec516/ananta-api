837f01c46d8bdd8ae41440cd279d5abda89cf263
jahidul <jsec516@gmail.com>
2015-06-19 22:52:15 +0600

    rbac tables added

---
+++ database/migrations/2015_06_19_164446_create_perm_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perm_role', function (Blueprint $table) {
+        	$table->integer('role_id')->unsigned();
+        	$table->integer('permission_id')->unsigned();
+            $table->integer('clinic_id')->unsigned();
+            $table->integer('clinic_user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('perm_role');
+    }
+}
---
+++ database/migrations/2015_06_19_164512_create_role_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->increments('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->integer('clinic_id')->unsigned();
+            $table->integer('clinic_user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('role_user');
+    }
+}
