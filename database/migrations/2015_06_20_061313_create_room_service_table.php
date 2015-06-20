9169aa0d98dbd3594906b74c3fa342b06557a320
jahidul <jsec516@gmail.com>
2015-06-20 12:18:33 +0600

    clinic-room-service related migration added

---
+++ database/migrations/2015_06_20_061313_create_room_service_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_service', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('room_service');
+    }
+}
