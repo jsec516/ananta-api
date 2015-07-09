b96a342937f6f1c56de584dad9440c741536832c
Jahidul Islam <jsec516@gmail.com>
2015-07-09 15:47:03 +0600

    gate keeper added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,31 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-$app->get('/', function() use ($app) {
-    return $app->welcome();
+ * |--------------------------------------------------------------------------
+ * | Application Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register all of the routes for an application.
+ * | It's a breeze. Simply tell Laravel the URIs it should respond to
+ * | and give it the controller to call when that URI is requested.
+ * |
+ */
+$app->get ( '/', function () use($app) {
+	return $app->welcome ();
 });
 
-	$app->get('user/', [
-	'as' => 'get_user', 'uses' => 'UserController@repoPractice'
+/*$app->get ( 'user/', [ 
+		'as' => 'get_user',
+		'uses' => 'UserController@repoPractice' 
+]);*/
+
+$app->post ( 'user/save', [ 
+		'as' => 'save_user',
+		'uses' => 'UserController@store' 
 ]);
 
-$app->post('user/save', [
-	'as' => 'save_user', 'uses' => 'UserController@store'
+$app->post ( 'clinic/save', [ 
+		'as' => 'save_clinic',
+		'uses' => 'ClinicController@store' 
 ]);
 
-$app->post('clinic/save', [
-		'as' => 'save_clinic', 'uses' => 'ClinicController@store'
-]);
